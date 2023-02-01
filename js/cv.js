bd1aad53e399f1e5daf079ea2bd05ceeb8a846d7
pabloarg88 <ramospabloarg@gmail.com>
2023-02-01 14:54:38 -0300

    Agregando Proyecto

---
+++ js/cv.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("direccion").addEventListener('click', function() {
+    window.location.assign("https://www.google.com.ar/maps/place/Av.+Pres.+Roque+S%C3%A1enz+Pe%C3%B1a+615,+C1035+CABA/@-34.6068253,-58.3775391,17z/data=!3m1!4b1!4m5!3m4!1s0x95bccad2696bb6c3:0x4800c48ae3d47d0d!8m2!3d-34.6068253!4d-58.3753504");
+})
+
+document.getElementById("email").addEventListener('click', function() {
+    window.location.assign("https://www.gmail.com");
+})
+
+document.getElementById("linkedin").addEventListener('click', function() {
+    window.location.assign("https://ar.linkedin.com/");
+})
